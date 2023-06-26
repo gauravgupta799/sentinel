aba9f29125cf088a75dad4b3204760dcf77d348c
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-06-26 20:59:31 +0530

    about-page-done&few-code-modified

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,10 @@
 const loader = document.querySelector(".page-loader");
 const header = document.querySelector(".header");
 const nav = document.querySelector(".nav");
-// const hamBtn =document.querySelector("#hamnurgur-btn");
+const mobileMenu = document.querySelector(".header__mobile-wrapper");
+const hamburgerBtn = document.querySelector(".burger__wrapper");
+const closeMenuBtn = document.querySelector(".close-btn");
+// const navItem = document.querySelectorAll(".nav__item");
 
 //====== Pre-loader start ======
 window.onload = () =>{
@@ -19,18 +22,89 @@ window.addEventListener("scroll", () => {
   });
 //====== Sticky header end ======
 
-// hamBtn.addEventListener("click", function(){
-//     nav.classList.add(".show")
+hamburgerBtn.addEventListener("click", function(){
+  mobileMenu.classList.add("show-menu");
+})
+
+closeMenuBtn.addEventListener("click", function(){
+  mobileMenu.classList.remove("show-menu");
+})
+
+//======  Active menu link start ======
+// navItem.forEach((item)=>{
+//   item.addEventListener("click", ()=>{
+//     item.classList.add("active");
+//   })
 // })
+//======  Active menu link end ======
+
+
 
 //====== Swiper start ======
 var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
+  // spaceBetween: 30,
   grabCursor: true,
   slidesPerView:1,
-  loop:true,
+  // loop:true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 15
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    992: {
+      slidesPerView: 1,
+      spaceBetween: 25
+    }
+  }
 });
+
+
+var swiper = new Swiper(".mySwiper--hero", {
+  // spaceBetween: 30,
+  grabCursor: true,
+  slidesPerView:1,
+  // loop:true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 15
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    992: {
+      slidesPerView: 1,
+      spaceBetween: 25
+    }
+  }
+});
+
+//====== Swiper end ======
